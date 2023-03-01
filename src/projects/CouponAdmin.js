196641b1a1ad6fbcd9ce3805c15f12cb5df6eac0
wookey <anbriaao@naver.com>
2023-03-01 21:46:06 +0900

    update

--- src/projects/CouponAdmin.js
+++ src/projects/CouponAdmin.js
@@ -28,16 +28,16 @@ const CouponAdmin = () => {
                         </blockquote>
                     </StyleContentsLeft>
                     <StyleContentsRight>
-                        <StyleContentsTitle>쿠폰의 수 많은 정책 정보 관리</StyleContentsTitle>
+                        <StyleContentsTitle>쿠폰의 정책 정보 관리</StyleContentsTitle>
                         <StyleContents>
                             <StyleContent>쿠폰 단건, 일괄, 발급 등을 지원할 수 있는 어드민 시스템 개발</StyleContent>
                             <StyleContent>쿠폰 데이터 용도에 따라 mysqlDB, redis, couchbase, rabbitMQ 등의 미들웨어 활용</StyleContent>
                         </StyleContents>
                         <StyleContentsTitle>레거시 관리</StyleContentsTitle>
                         <StyleContents>
-                            <StyleContent>60개 이상의 변동되는 쿠폰 정책의 무분별 로직들을 캡슐화하고 역할과 책임을 기반으로 레거시 코드 관리</StyleContent>
-                            <StyleContent>쿠폰 정책을 TDD/BDD 기반으로 단일 책임 수준의 검증이 가능하도록 리펙토링</StyleContent>
-                            <StyleContent>최대 60s이 소요된 레거시 어드민 관리 페이지를 10ms 수준으로 리펙토링</StyleContent>
+                            <StyleContent>60개 이상의 쿠폰 정책 로직을 캡슐화하고 역할과 책임이 명확한 구조로 재설계</StyleContent>
+                            <StyleContent>TDD/BDD 기반으로 단일 책임 수준의 검증이 가능하도록 리펙토링</StyleContent>
+                            <StyleContent>응답시간이 60s 소요된 레거시 어드민 관리 페이지를 10ms 수준으로 변경</StyleContent>
                         </StyleContents>
                     </StyleContentsRight>
                 </StyleContentsArea>
